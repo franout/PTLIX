c25628109aaeb29a49e7bbb410ead928f54d2e6b
francesco <francesco.angione@polito.it>
2025-05-07 12:16:06 +0200

    feat: addingadditional defines

--- cfg/stl_cfg.h
+++ cfg/stl_cfg.h
@@ -37,8 +37,8 @@
 
 #define STL_SIGNATURE_SECTION ".stl_rt_code"
 
-#define STL_CODE_SECTION_MODIFIED_IVOR ".text_vle"
-#define STL_CODE_SECTION_MODIFIED_HANDLERS ".text_vle"
+#define STL_CODE_SECTION_MODIFIED_EXCEPTION ".stl_exception_table"
+#define STL_CODE_SECTION_MODIFIED_HANDLERS ".stl_exception_handlers"
 /**@}*/
 
 
@@ -52,8 +52,8 @@
 
 /** 
  *  Note Scheduler type:
- *                       - 0 Sequential SBST scheduler
- *                       - 1 Not implemented
+ *                       - 0 Sequential SBST scheduler (for runtime tests only)
+ *                       - 1 Chunk-based SBST scheduler (for runtime tests only)
  *                       - 2 Not implemented
 */
 #define STL_SCHEDULER_TYPE                      0u 
@@ -67,9 +67,31 @@
 
 #define STL_MULTICORE_SOC                       0u
 
+
+/* CPU related*/
 #if (STL_MULTICORE_SOC > 0u)
     #define STL_MULTICORE_EXECUTION 		    0u 
+    #define STL_NUM_CPU                         2u 
+    #define STL_USE_MPU                         0u /* Use the memory protection unit */
 #endif /*STL_MULTICORE_SOC*/
+
+
+#define STL_USE_WATCHDOG                        0u /* Use the watchdog for test execution */
+#define STL_USE_FINE_GRAINED_WATCHDOG           0u /* Use the fine grained watchdog for test execution (defined for each test in the STL) */
+#if (STL_USE_WATCHDOG > 0u && STL_USE_FINE_GRAINED_WATCHDOG == 0u)
+    #define STL_WATCHDOG_TIMEOUT                0x00000000u /* Watchdog timeout value */
+    #define STL_WATCHDOG_RESET_VALUE            0u /* Watchdog reset value */
+#endif /*STL_USE_WATCHDOG*/
+
+/* CSP related*/
+#define STL_
+/* OS related*/
+#if (STL_OS_PRESENT > 0u)
+    #define STL_OS_RT_TASK_STACK_SIZE          0x00000000u /* Stack size for the OS task */
+    #define STL_OS_RT_TASK_PRIORITY            0u /* Task priority for the OS task */
+    #define STL_OS_RT_TASK_PERIOD              0u /* Task period for the OS task */
+    #define STL_OS_RT_TASK_CRC                 0u /* Compute CRC for the OS task in charge of executing the STL */
+#endif /* SLT_OS_PRESENT */
 
 /*****************************************************************************************************/
 /****************                    Software-Self Tests                              ****************/
@@ -80,17 +102,19 @@
 #define STL_RUNTIME_TEST 	                    0u 
 
 
+/*****************************************************************************************************/
 /*************************** Signature related defines ***********************************************/
-//TODO signature mismatch
+/*****************************************************************************************************/
 /*! mismatch value if cocmputed signature is different from the golden signature*/
 #define STL_SIGNATURE_MISMATCH 		0x01010101
 
-// TODO change polito spyaddr and test code ram base
-#define POLITO_SPYADDR  __stl_polito_spy_addr__
-#define TEST_CODE_RAM_BASE __stl_TEST_CODE_RAM_BASE__
+
+/*****************************************************************************************************/
+/*************************** Relocation related defines **********************************************/
+/*****************************************************************************************************/
 
 #define STL_RELOCATED 					0u  //TODO verify relocation
-#define STL_RELOCATION_TABLE			0u
+#define STL_RELOCATION_TABLE			0u  /* Relocation table for multiple relocations */
 
 /*****************************************************************************************************/
 /****************                    Error Management                                 ****************/
