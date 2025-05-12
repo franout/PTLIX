effb2543fb16e948414c49568702a4f25addb712
franout <francesco.angione@polito.it>
2025-05-12 23:45:42 +0200

    feat: adding skeleton for test setup support package

--- TSSP/OS/template/stl_al_os.c
+++ TSSP/OS/template/stl_al_os.c
@@ -1,4 +1,14 @@
 #if __STL__
+#include "stl_cfg.h"
+#include "stl_types.h"
+#include "stl_al_os.h"
+
+#ifndef STL_AL_OS_MODULE
+#define STL_AL_OS_MODULE
 
 
+
+
+
+#endif /* STL_AL_OS_MODULE */
 #endif /*__STL__*/
