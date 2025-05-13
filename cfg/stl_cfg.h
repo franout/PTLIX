bba0094e3773180f412698e45210466cbdfad16d
franout <francesco.angione@polito.it>
2025-05-13 19:15:24 +0200

    feat: commenting and refactoring

--- cfg/stl_cfg.h
+++ cfg/stl_cfg.h
@@ -1,3 +1,54 @@
+/**
+ * @file stl_cfg.h
+ * @brief Configuration header for the Software Test Library (STL).
+ *
+ * This file contains configuration macros and definitions for enabling and
+ * customizing the Software Test Library (STL). It includes compiler-dependent
+ * directives, linker section definitions, scheduler services, test setup
+ * support, and error management configurations.
+ *
+ * @note The macro `__STL__` must be defined at the compiler level to enable
+ *       compilation of the STL.
+ *
+ * @section CompilerDirectives Compiler Dependent Directives
+ * - Defines keywords for extended assembly, weak symbols, static, and inline
+ *   based on the compiler being used.
+ * - Issues a warning if the compiler is not recognized.
+
+ * @section LinkerSections Linker Dependent Sections
+ * - Defines custom linker sections for STL boot-time and runtime code, data,
+ *   and exception handlers.
+ * - These sections must be instantiated in the linker script.
+
+ * @section SchedulerServices Scheduler Services
+ * - Defines the chunk size and scheduler type for runtime tests.
+ * - Scheduler types:
+ *   - 0: Sequential SBST scheduler (runtime tests only).
+ *   - 1: Chunk-based SBST scheduler (runtime tests only).
+ *   - 2: Custom scheduler (user-defined).
+
+ * @section TestSetup Test Setup Support Package
+ * - Configures options for OS presence, multicore SoC, memory protection unit
+ *   (MPU), and watchdog usage.
+ * - Includes settings for OS task stack size, priority, period, and CRC.
+
+ * @section SoftwareSelfTests Software-Self Tests
+ * - Enables or disables boot-time and runtime tests.
+
+ * @section Signature Signature Related Defines
+ * - Defines the mismatch value for signature verification.
+
+ * @section Relocation Relocation Related Defines
+ * - Configures relocation settings and relocation table usage.
+
+ * @section ErrorManagement Error Management
+ * - Enables or disables error management and verbose error reporting.
+
+ * @section ErrorCheck Error Check
+ * - Ensures at least one test type (boot-time or runtime) is selected.
+ *
+ * @warning If no tests are selected, a compilation error will occur.
+ */
 #ifndef __STL_CFG_H__
 #define __STL_CFG_H__
 
@@ -5,17 +56,16 @@
 /**
  * Define for enabling compilation of Software Test Library.
  * Note: It must be defined at compiler level.
-*/
-#define __STL__								1u
+ */
+#define __STL__ 1u
 #endif /*__STL__*/
 
-
 /************************ Compiler Dependent directives ************************/
-/** 
+/**
  *   Assembly keyword for Extended assembly.
  */
 #if defined(__GNUC__) || defined(__clang__)
-#define ASM_KEYWORD(x) asm volatile(x) 
+#define ASM_KEYWORD(x) asm volatile(x)
 #define WEAK_KEYWORD __attribute__((weak))
 #define STATIC_KEYWORD static
 #define INLINE_KEYWORD inline
@@ -25,7 +75,7 @@
 /**
  * \defgroup Linker dependent sections.
  * Section definition for library (they must be instantiated in the linker script)
- * 
+ *
  * Note: If not defines in the linker script, the STL goes to the end of all the other sections
  */
 /**@{*/
@@ -47,88 +97,82 @@
 #define STL_CODE_SECTION_MODIFIED_HANDLERS ".stl_exception_handlers"
 /**@}*/
 
-
 /*****************************************************************************************************/
 /****************                         Scheduler services                          ****************/
 /****************                                                                     ****************/
 /*****************************************************************************************************/
 
-#define STL_TEST_CHUNK_SIZE                     1u
+#define STL_TEST_CHUNK_SIZE 1u
 
-
-/** 
+/**
  *  Note Scheduler type:
  *                       - 0 Sequential SBST scheduler (for runtime tests only)
  *                       - 1 Chunk-based SBST scheduler (for runtime tests only)
- *                       - 2 Custom
-*/
-#define STL_SCHEDULER_TYPE                      0u 
+ *                       - 2 Custom (overwrite the definition)
+ */
+#define STL_SCHEDULER_TYPE 0u
 
 /*****************************************************************************************************/
 /****************                    Test Setup Support Package                       ****************/
 /****************                                                                     ****************/
 /*****************************************************************************************************/
 
-#define STL_OS_PRESENT					        0u 
+#define STL_OS_PRESENT 0u
 
-#define STL_MULTICORE_SOC                       0u
-
+#define STL_MULTICORE_SOC 0u
 
 /* CPU related*/
 #if (STL_MULTICORE_SOC > 0u)
-    #define STL_MULTICORE_EXECUTION 		    0u 
-    #define STL_NUM_CPU                         2u 
-    #define STL_USE_MPU                         0u /* Use the memory protection unit */
-#endif /*STL_MULTICORE_SOC*/
+#define STL_MULTICORE_EXECUTION 0u
+#define STL_NUM_CPU 2u
+#define STL_USE_MPU 0u /* Use the memory protection unit */
+#endif				   /*STL_MULTICORE_SOC*/
 
 /* CSP related*/
-#define STL_USE_WATCHDOG                        0u /* Use the watchdog for test execution */
-#define STL_USE_FINE_GRAINED_WATCHDOG           0u /* Use the fine grained watchdog for test execution (defined for each test in the STL) */
+#define STL_USE_WATCHDOG 0u /* Use the watchdog for test execution */
+#define STL_USE_FINE_GRAINED_WATCHDOG                                                                                  \
+	0u /* Use the fine grained watchdog for test execution (defined for each test in the STL) */
 #if (STL_USE_WATCHDOG > 0u && STL_USE_FINE_GRAINED_WATCHDOG == 0u)
-    #define STL_WATCHDOG_TIMEOUT                0x00000000u /* Watchdog timeout value */
-    #define STL_WATCHDOG_RESET_VALUE            0u /* Watchdog reset value */
-#endif /*STL_USE_WATCHDOG*/
-
+#define STL_WATCHDOG_TIMEOUT 0x00000000u /* Watchdog timeout value */
+#define STL_WATCHDOG_RESET_VALUE 0u		 /* Watchdog reset value */
+#endif									 /*STL_USE_WATCHDOG*/
 
 /* OS related*/
 #if (STL_OS_PRESENT > 0u)
-    #define STL_OS_RT_TASK_STACK_SIZE          0x00000000u /* Stack size for the OS task */
-    #define STL_OS_RT_TASK_PRIORITY            0u /* Task priority for the OS task */
-    #define STL_OS_RT_TASK_PERIOD              0u /* Task period for the OS task */
-    #define STL_OS_RT_TASK_CRC                 0u /* Compute CRC for the OS task in charge of executing the STL */
-#endif /* SLT_OS_PRESENT */
+#define STL_OS_RT_TASK_STACK_SIZE 0x00000000u /* Stack size for the OS task */
+#define STL_OS_RT_TASK_PRIORITY 0u			  /* Task priority for the OS task */
+#define STL_OS_RT_TASK_PERIOD 0u			  /* Task period for the OS task */
+#define STL_OS_RT_TASK_CRC 0u				  /* Compute CRC for the OS task in charge of executing the STL */
+#endif										  /* SLT_OS_PRESENT */
 
 /*****************************************************************************************************/
 /****************                    Software-Self Tests                              ****************/
 /****************                                                                     ****************/
 /*****************************************************************************************************/
 
-#define STL_BOOT_TEST 		                    0u 
-#define STL_RUNTIME_TEST 	                    0u 
-
+#define STL_BOOT_TEST 0u
+#define STL_RUNTIME_TEST 0u
 
 /*****************************************************************************************************/
 /*************************** Signature related defines ***********************************************/
 /*****************************************************************************************************/
 /*! mismatch value if cocmputed signature is different from the golden signature*/
-#define STL_SIGNATURE_MISMATCH 		0x01010101
-
+#define STL_SIGNATURE_MISMATCH 0x01010101
 
 /*****************************************************************************************************/
 /*************************** Relocation related defines **********************************************/
 /*****************************************************************************************************/
 
-#define STL_RELOCATED 					0u  //TODO verify relocation
-#define STL_RELOCATION_TABLE			0u  /* Relocation table for multiple relocations */
+#define STL_RELOCATED 0u		// TODO verify relocation
+#define STL_RELOCATION_TABLE 0u /* Relocation table for multiple relocations */
 
 /*****************************************************************************************************/
 /****************                    Error Management                                 ****************/
 /****************                                                                     ****************/
 /*****************************************************************************************************/
 
-#define STL_ERROR_MANAGEMENT_ENABLED	1u 
-#define STL_ERROR_MANAGEMENT_VERBOSE    0u 
-
+#define STL_ERROR_MANAGEMENT_ENABLED 1u
+#define STL_ERROR_MANAGEMENT_VERBOSE 0u
 
 /*****************************************************************************************************/
 /****************                    Error Check                                      ****************/
@@ -137,6 +181,6 @@
 
 #if (STL_BOOT_TEST == 0u && STL_RUNTIME_TEST == 0u)
 #error "No tests selected. Please select at least one test type."
-#endif 
+#endif
 
 #endif /* __STL_CFG_H__ */
--- stl_types.h
+++ stl_types.h
@@ -1,56 +1,169 @@
 #ifndef __STL_TYPES_H__
 #define __STL_TYPES_H__
 
+/**
+ * @file stl_types.h
+ * @brief This header file defines common types, constants, and error codes used in the STL (Self-Test Library).
+ *
+ * It includes definitions for boolean values, null pointers, error codes, and various type aliases
+ * used throughout the STL. Additionally, it provides type definitions for function pointers and
+ * CPU identifiers in multicore systems.
+ */
 
+/**
+ * @def STL_TRUE
+ * @brief Macro representing a boolean true value.
+ */
+
+/**
+ * @def STL_FALSE
+ * @brief Macro representing a boolean false value.
+ */
+
+/**
+ * @def SLT_NULL
+ * @brief Macro representing a null pointer value.
+ */
+
+/**
+ * @typedef STL_SIGNATURE_T
+ * @brief Type used for signature values in STL operations.
+ */
+
+/**
+ * @enum STL_ERROR_T
+ * @brief Enumeration of error codes for STL operations.
+ *
+ * This enumeration defines various error codes that can be returned by STL functions.
+ * Each error code corresponds to a specific type of error encountered during operation.
+ *
+ * @var STL_ERROR_T::STL_ERROR_NONE
+ * No error occurred.
+ *
+ * @var STL_ERROR_T::STL_ERROR_NOT_IMPLEMENTED
+ * The requested feature is not implemented.
+ *
+ * @var STL_ERROR_T::STL_ERROR_TASK_ALLOCATION
+ * Error occurred during task allocation.
+ *
+ * @var STL_ERROR_T::STL_ERROR_RELOCATION
+ * Error occurred during relocation.
+ *
+ * @var STL_ERROR_T::STL_ERROR_SIG_MISMATCH
+ * Signature mismatch error.
+ *
+ * @var STL_ERROR_T::STL_ERROR_INDEX
+ * Index error.
+ *
+ * @var STL_ERROR_T::STL_ERROR_SBST
+ * Subsystem error.
+ *
+ * @var STL_ERROR_T::STL_CPU_OUT_OF_BOUNDS
+ * CPU index is out of bounds.
+ *
+ * @var STL_ERROR_T::STL_INDEX_OUT_OF_BOUNDS
+ * Index is out of bounds.
+ *
+ * @var STL_ERROR_T::SLT_NO_RT_ROUTINE
+ * No runtime routine is available.
+ *
+ * @var STL_ERROR_T::SLT_NO_BT_ROUTINE
+ * No boot-time routine is available.
+ *
+ * @var STL_ERROR_T::STL_ERROR_CUSTOM_SCHEDULER_NOT_IMPLEMENTED
+ * Custom scheduler is not implemented.
+ */
+
+/**
+ * @typedef STL_BOOL
+ * @brief Boolean type used in STL operations.
+ */
+
+/**
+ * @typedef STL_SIZE_T
+ * @brief Type used to represent sizes in STL operations.
+ */
+
+/**
+ * @typedef SLT_INT32U_T
+ * @brief Unsigned 32-bit integer type used in STL operations.
+ */
+
+/**
+ * @typedef STL_ADDR_T
+ * @brief Type used to represent addresses in STL operations.
+ */
+
+/**
+ * @typedef STL_CPUS
+ * @brief Type used to define the CPU number in a multicore system.
+ *
+ * This type is used to identify the CPU on which the test is running. It is utilized
+ * in the STL_schedule_runtime and STL_schedule_bootime functions.
+ */
+
+/**
+ * @typedef STL_FUNCT_PTR_T
+ * @brief Function pointer type for STL functions.
+ *
+ * This type represents a pointer to a function that returns an STL_SIGNATURE_T value.
+ */
 #include "typedefs.h"
 
+// Boolean definitions
+#define STL_TRUE 1u
+#define STL_FALSE 0u
 
-#define STL_TRUE 						1u
-#define STL_FALSE 						0u
+// Null pointer definition
+#define SLT_NULL 0x00000000UL
 
-#define SLT_NULL						0x00000000UL
-
+// Type for signature values
 typedef int STL_SIGNATURE_T;
 
-typedef enum {
-	STL_ERROR_NONE=0,
-	STL_ERROR_NOT_IMPLEMENTED=2,
-	STL_ERROR_TASK_ALLOCATION=10,
+// Error codes for STL operations
+typedef enum
+{
+	STL_ERROR_NONE = 0,				// No error
+	STL_ERROR_NOT_IMPLEMENTED = 2,	// Feature not implemented
+	STL_ERROR_TASK_ALLOCATION = 10, // Task allocation error
 
-	STL_ERROR_RELOCATION=20,
+	STL_ERROR_RELOCATION = 20, // Relocation error
 
-	STL_ERROR_SIG_MISMATCH=30,
+	STL_ERROR_SIG_MISMATCH = 30, // Signature mismatch error
 
+	STL_ERROR_INDEX = 40, // Index error
 
-	STL_ERROR_INDEX=40,
+	STL_ERROR_SBST = 50, // Subsystem error
 
+	STL_CPU_OUT_OF_BOUNDS = 60,	  // CPU index out of bounds
+	STL_INDEX_OUT_OF_BOUNDS = 70, // Index out of bounds
 
-	STL_ERROR_SBST=50,
+	SLT_NO_RT_ROUTINE = 80, // No runtime routine available
+	SLT_NO_BT_ROUTINE = 90, // No boot-time routine available
 
-
-	STL_CPU_OUT_OF_BOUNDS=60,
-	STL_INDEX_OUT_OF_BOUNDS=70,
-
-	SLT_NO_RT_ROUTINE=80,
-	SLT_NO_BT_ROUTINE=90
+	STL_ERROR_CUSTOM_SCHEDULER_NOT_IMPLEMENTED = 100 // Custom scheduler not implemented
 } STL_ERROR_T;
 
-
+// Boolean type
 typedef char STL_BOOL;
 
+// Size type for STL operations
 typedef uint16_t STL_SIZE_T;
+
+// Unsigned 32-bit integer type
 typedef uint32_t SLT_INT32U_T;
 
+// Address type
 typedef uint32_t STL_ADDR_T;
+
 /**
  * @brief STL_CPUS is used to define the CPU number in a multicore system.
  * It is used to identify the CPU on which the test is running.
- * It is used in the STL_schedule_runtime and STL_schedule_bootime functions.
- * It is used to identify the CPU on which the test is running.
+ * This is utilized in the STL_schedule_runtime and STL_schedule_bootime functions.
  */
 typedef uint8_t STL_CPUS;
 
+// Function pointer type for STL functions
 typedef STL_SIGNATURE_T (*STL_FUNCT_PTR_T)(void);
 
-
-#endif /* __STL_TYPES_H__*/
+#endif /* __STL_TYPES_H__ */
